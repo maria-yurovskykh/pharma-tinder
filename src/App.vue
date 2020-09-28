cef846d73e2f64c244c7c00cc913d273402603b1
Maria <makasyamka@gmail.com>
2020-09-28 13:29:10 +0300

    Refactor styles

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <div class="pharm-tinder">
     <transition name="fade">
       <div v-if="step === 1" class="home" key="1">
-        <div class="home__circle"></div>
-        <h1 class="home__heading">
-          Допоможи
-          <span class="heading__pharmacist">фармацевту</span>
-        </h1>
-        <h2 class="home__welcome">Ласкаво просимо y гру</h2>
-        <button class="home__start" @click="setStep(2)">Розпочати</button>
+        <div class="home__circle">
+          <p class="home__welcome">Ласкаво просимо y гру</p>
+          <p class="home__heading">
+            Допоможи
+            <span class="heading__pharmacist">фармацевту</span>
+          </p>
+          <button class="home__start" @click="setStep(2)">Розпочати</button>
+        </div>
       </div>
 
       <div v-else-if="step === 2" class="game" key="2">
@@ -16,12 +17,14 @@
           <button class="side-bar__home" @click="setStep(1)"></button>
           <button class="side-bar__retry" @click="setStep(2)"></button>
           <p class="side-bar__parameters">Параметри:</p>
-          <span class="side-bar__parameter --sad">{{sad}}</span>
-          <span class="side-bar__parameter --happy">{{happy}}</span>
-          <span class="side-bar__parameter --heart">{{heart}}</span>
+          <span class="side-bar__parameter --sad">{{ sad }}</span>
+          <span class="side-bar__parameter --happy">{{ happy }}</span>
+          <span class="side-bar__parameter --heart">{{ heart }}</span>
           <div class="side-bar__counter">
             <p class="counter__text">Залишилось у черзі:</p>
-            <p class="counter__left">{{queueCount}}/{{clientsDataLength}}</p>
+            <p class="counter__left">
+              {{ queueCount }}/{{ clientsDataLength }}
+            </p>
           </div>
         </div>
 
@@ -36,24 +39,34 @@
             <div
               class="screen__card"
               :class="{
-                'purple': throwoutleft,
-                'blue': throwoutup,
-                'yellow': throwoutright,
+                purple: throwoutleft,
+                blue: throwoutup,
+                yellow: throwoutright,
               }"
               v-for="(client, index) in clients"
               :key="index"
             >
               <div class="card__image-wrapper">
-                <img class="card__image" :src="client.img" alt="Client's photo" />
+                <img
+                  class="card__image"
+                  :src="client.img"
+                  alt="Client's photo"
+                />
                 <div class="card__gradient"></div>
               </div>
-              <p class="card__heading">{{client.heading}}</p>
-              <p class="card__content">{{client.problem}}</p>
+              <p class="card__heading">{{ client.heading }}</p>
+              <p class="card__content">{{ client.problem }}</p>
             </div>
           </vue-swing>
-          <button class="screen__button --purple" @click="throwleft()">Препарат 1</button>
-          <button class="screen__button --blue" @click="throwup()">Препарат 2</button>
-          <button class="screen__button --yellow" @click="throwright()">Препарат 3</button>
+          <button class="screen__button --purple" @click="throwleft()">
+            Препарат 1
+          </button>
+          <button class="screen__button --blue" @click="throwup()">
+            Препарат 2
+          </button>
+          <button class="screen__button --yellow" @click="throwright()">
+            Препарат 3
+          </button>
         </div>
       </div>
 
@@ -61,9 +74,15 @@
       <div v-else-if="step === 3" class="final" key="3">
         <button class="final__home" @click="setStep(1)"></button>
         <div class="final__counter">
-          <div class="counter__percents --drug-1">{{countPercentOfSales(this.sad)}}%</div>
-          <div class="counter__percents --drug-2">{{countPercentOfSales(this.happy)}}%</div>
-          <div class="counter__percents --drug-3">{{countPercentOfSales(this.heart)}}%</div>
+          <div class="counter__percents --drug-1">
+            {{ countPercentOfSales(this.sad) }}%
+          </div>
+          <div class="counter__percents --drug-2">
+            {{ countPercentOfSales(this.happy) }}%
+          </div>
+          <div class="counter__percents --drug-3">
+            {{ countPercentOfSales(this.heart) }}%
+          </div>
           <div class="counter__drug --one">Препарат 1</div>
           <div class="counter__drug --two">Препарат 2</div>
           <div class="counter__drug --three">Препарат 3</div>
@@ -71,11 +90,14 @@
         <div class="final__result">
           <p class="result__your-result">Ваш результат:</p>
           <p class="result__result">«Що я тут роблю?»</p>
-          <p
-            class="result__content"
-          >Це тестове завдання, так що не будемо заглиблюватися в глибини проблем фармацевтів.</p>
+          <p class="result__content">
+            Це тестове завдання, так що не будемо заглиблюватися в глибини
+            проблем фармацевтів.
+          </p>
         </div>
-        <button class="final__restart" @click="setStep(2)">Спробувати ще</button>
+        <button class="final__restart" @click="setStep(2)">
+          Спробувати ще
+        </button>
       </div>
     </transition>
   </div>
